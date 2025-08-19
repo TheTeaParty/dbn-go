8fd07bdca5f2d1396b20a3307ef560787db9cfc8
Vlad Gorokhov <vlad.gorokh@gmail.com>
2025-08-19 14:50:34 +0100

    test fixed

--- hist/hist_test.go
+++ hist/hist_test.go
@@ -3,11 +3,12 @@
 package dbn_hist
 
 import (
+	"context"
 	"os"
 	"testing"
 	"time"
 
-	dbn "github.com/NimbleMarkets/dbn-go"
+	"github.com/NimbleMarkets/dbn-go"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -20,38 +21,43 @@ func TestDbnHist(t *testing.T) {
 }
 
 var databentoApiKey string
+var cli Client
+var ctx context.Context
 
 var _ = BeforeSuite(func() {
 	databentoApiKey = os.Getenv("DATABENTO_API_KEY")
 	if databentoApiKey == "" {
 		Fail("DATABENTO_API_KEY not set")
 	}
+
+	ctx = context.Background()
+	cli = New(databentoApiKey, nil)
 })
 
 var _ = Describe("DbnHist", func() {
 	Context("metadata", func() {
 		It("should ListPublishers", func() {
-			publishers, err := ListPublishers(databentoApiKey)
+			publishers, err := cli.ListPublishers(ctx)
 			Expect(err).To(BeNil())
 			Expect(publishers).ToNot(BeEmpty())
 		})
 		It("should ListDatasets, ListSchemas, ListFields, ListUnitPrices", func() {
-			datasets, err := ListDatasets(databentoApiKey, DateRange{})
+			datasets, err := cli.ListDatasets(ctx, DateRange{})
 			Expect(err).To(BeNil())
 			Expect(datasets).ToNot(BeEmpty())
 
-			schemas, err := ListSchemas(databentoApiKey, datasets[0])
+			schemas, err := cli.ListSchemas(ctx, datasets[0])
 			Expect(err).To(BeNil())
 			Expect(schemas).ToNot(BeEmpty())
 
 			schema, err := dbn.SchemaFromString(schemas[0])
 			Expect(err).To(BeNil())
 
-			fields, err := ListFields(databentoApiKey, dbn.Encoding_Dbn, schema)
+			fields, err := cli.ListFields(ctx, dbn.Encoding_Dbn, schema)
 			Expect(err).To(BeNil())
 			Expect(fields).ToNot(BeEmpty())
 
-			unitPrices, err := ListUnitPrices(databentoApiKey, datasets[0])
+			unitPrices, err := cli.ListUnitPrices(ctx, datasets[0])
 			Expect(err).To(BeNil())
 			Expect(unitPrices).ToNot(BeEmpty())
 		})
@@ -68,24 +74,24 @@ var _ = Describe("DbnHist", func() {
 				},
 			}
 
-			conditions, err := GetDatasetCondition(databentoApiKey, metaParams.Dataset, metaParams.DateRange)
+			conditions, err := cli.GetDatasetCondition(ctx, metaParams.Dataset, metaParams.DateRange)
 			Expect(err).To(BeNil())
 			Expect(conditions).ToNot(BeEmpty())
 
-			dateRange, err := GetDatasetRange(databentoApiKey, metaParams.Dataset)
+			dateRange, err := cli.GetDatasetRange(ctx, metaParams.Dataset)
 			Expect(err).To(BeNil())
 			Expect(dateRange.Start).ToNot(Equal(time.Time{}))
 			Expect(dateRange.End).ToNot(Equal(time.Time{}))
 
-			recordCount, err := GetRecordCount(databentoApiKey, metaParams)
+			recordCount, err := cli.GetRecordCount(ctx, metaParams)
 			Expect(err).To(BeNil())
 			Expect(recordCount).To(BeNumerically(">", 0))
 
-			billableSize, err := GetBillableSize(databentoApiKey, metaParams)
+			billableSize, err := cli.GetBillableSize(ctx, metaParams)
 			Expect(err).To(BeNil())
 			Expect(billableSize).To(BeNumerically(">", 0))
 
-			cost, err := GetCost(databentoApiKey, metaParams)
+			cost, err := cli.GetCost(ctx, metaParams)
 			Expect(err).To(BeNil())
 			Expect(cost).To(BeNumerically(">", 0))
 		})
@@ -100,7 +106,7 @@ var _ = Describe("DbnHist", func() {
 					End:   time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
 				},
 			}
-			resolveResp, err := SymbologyResolve(databentoApiKey, resolveParams)
+			resolveResp, err := cli.SymbologyResolve(ctx, resolveParams)
 			Expect(err).To(BeNil())
 			Expect(resolveResp).ToNot(BeNil())
 			Expect(resolveResp.Mappings).ToNot(BeEmpty())
